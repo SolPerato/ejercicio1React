890846aa19b636c2b5167bb7bddbf64316f7d669
Sol Perato <solperato27@gmail.com>
2023-06-01 00:05:52 -0300

    Agregue el Hola Mundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Titulos from './components/titulos'
+
 
 function App() {
 
---
+++ src/components/titulos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titulos = () => {
+    return (
+        <section className='Text-center'>
+         <h1>Hello World!</h1>            
+        </section>
+    );
+};
+
+export default Titulos;
